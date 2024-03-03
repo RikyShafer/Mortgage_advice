1cebcc6fb77b1afb9b7202fb7577b4f0daf0bd98
RikyShafer <r8524409@gmail.com>
2024-03-04 01:56:58 +0200

    new commit

--- client/src/features/auth/registeration/ProcessCompletionMessage.js
+++ client/src/features/auth/registeration/ProcessCompletionMessage.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { FiCheckCircle } from "react-icons/fi";
 
 
+
 const ProcessCompletionMessage = () => {
     const navigate = useNavigate();
 
