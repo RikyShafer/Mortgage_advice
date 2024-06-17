9c237b1d604eb6c6477bf5c934200ba318d65eb0
RikyShafer <r8524409@gmail.com>
2024-06-17 19:44:06 +0300

    adding links to the footer for Macion on the map / WhatsApp / email

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -7,19 +7,35 @@ import {
 
 
 import "./footer.css"
+import { NavLink } from 'react-router-dom';
 const Footer = () => {
+  const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=7673297@gmail.com&su=Contact%20Us&body=Hello,%20I%20would%20like%20to%20inquire%20about...`;
+  const phoneNumber = '0522279392'; // Replace with your phone number
+  const message = 'Hello, I would like to inquire about...'; // Replace with your message
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
   return (
     <div className='footer'>
       {/* <div className='footer-logo'>Riki</div>
       <div className='footer-text'> @ All Rikights reserved.</div> */}
      
-      <h1 className='footer-address'>  <BiMap /> כתבתינו: מסילת יוסף 9  </h1>
+      <h1 className='footer-address'>   
+      <NavLink to={"https://www.google.com/maps/place/%D7%9E%D7%A1%D7%99%D7%9C%D7%AA+%D7%99%D7%95%D7%A1%D7%A3+9,+%D7%9E%D7%95%D7%93%D7%99%D7%A2%D7%99%D7%9F+%D7%A2%D7%99%D7%9C%D7%99%D7%AA%E2%80%AD/@31.9301613,35.0467891,17z/data=!3m1!4b1!4m6!3m5!1s0x1502d2a18aec3c71:0xcece53b5cc888d90!8m2!3d31.9301613!4d35.0442195!16s%2Fg%2F12hr4dw8h?hl=iw&entry=ttu"} target="_blank"><BiMap /> כתבתינו: מסילת יוסף 9</NavLink>
+      </h1>
       <h1 className='studio'>  אפיון ועיצוב אתר | סטודיו מוריה| כל הזכויות שמורות </h1>
       <h1 className='footer-contactus'>  <BiLogoFacebook /> </h1>
+
       <h1 className='footer-contactus'>  <BiLogoInstagram /> </h1>
-      <h1 className='footer-contactus'>  <BiLogoWhatsapp /> </h1>
-      <h1 className='footer-contactus'>  <HiMail /> </h1>
-      </div>
+      <h1 className='footer-contactus'>
+        <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
+          <BiLogoWhatsapp />
+        </a>
+      </h1>
+      <h1 className='footer-contactus'>
+        <a href={gmailUrl} target="_blank" rel="noopener noreferrer">
+          <HiMail />
+        </a>
+      </h1>    </div>
   )
 }
 
